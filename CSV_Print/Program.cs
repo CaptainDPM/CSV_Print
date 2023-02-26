8b305dfe9436b24783f25bc0c887929c73836706
CaptainDPM <100872653+CaptainDPM@users.noreply.github.com>
2023-02-25 15:04:00 -1000

    Initial Commit

---
+++ CSV_Print/Program.cs
@@ -0,0 +1,4 @@
+﻿//Use a CSV file to print to console//
+
+ReadCSV readCSV = new ReadCSV();
+readCSV.Read();
---
+++ CSV_Print/ReadCSV.cs
@@ -0,0 +1,29 @@
+﻿public class ReadCSV
+{
+    public void Read()
+    {
+        //Use a CSV file to print to console//
+
+        string path = @"C:\addresses.csv";
+
+        List<string[]> data = new List<string[]>();
+
+        using (StreamReader streamReader = new StreamReader(path))
+        {
+            while (!streamReader.EndOfStream)
+            {
+                string[] line = streamReader.ReadLine().Split(',');
+                data.Add(line);
+            }
+        }
+        foreach (string[] field in data)
+        {
+            foreach (string value in field)
+            {
+                Console.Write(value + "\t");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
