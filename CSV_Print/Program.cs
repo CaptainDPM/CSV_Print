476e1283a5746932fdfe45dd42e3c3df37a6e421
CaptainDPM <100872653+CaptainDPM@users.noreply.github.com>
2023-03-09 18:24:35 -1000

    Added a user input

--- CSV_Print/Program.cs
+++ CSV_Print/Program.cs
@@ -1,5 +1,7 @@
 ﻿//Use a CSV file to print to console//
 
+TakeInput takeInput = new TakeInput();
+takeInput.Input();
 ReadCSV readCSV = new ReadCSV();
 readCSV.Read();
 //WriteCSV writeCSV = new WriteCSV();
---
+++ CSV_Print/TakeInput.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class TakeInput
+{
+    public void Input()
+    {
+        string path = @"C:\addresses.csv";
+
+        Console.WriteLine("Please enter first name.");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("Please enter last name.");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("Please enter street address.");
+        string streetAddress = Console.ReadLine();
+        Console.WriteLine("Please enter city.");
+        string city = Console.ReadLine();
+        Console.WriteLine("Please enter state.");
+        string state = Console.ReadLine();
+        Console.WriteLine("Please enter a random number.");
+        string lastNumber = Console.ReadLine();
+
+        string[] data = { firstName, lastName, streetAddress, city, state, lastNumber };
+
+        using (StreamWriter writer = new StreamWriter(path, true))
+        {
+            writer.WriteLine(string.Join(",", data));
+        }
+
+        Console.WriteLine("Data successfully written.");
+
+    }
+}
